3dbce4b0d6288f6259dd12edadedfce073a970b8
Phobos72 <120632821+Phobos72@users.noreply.github.com>
2022-12-20 18:05:53 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿//  1 задание
+Console.WriteLine("Input first number");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int b =Convert.ToInt32(Console.ReadLine());
+
+if (a<b)
+{
+    Console.WriteLine($"max={b}");
+}
+else
+{
+    Console.WriteLine($"max={a}");
+}
+
+//  2 задание
+Console.WriteLine("Input first number");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int b =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number");
+int c =Convert.ToInt32(Console.ReadLine());
+
+
+if (a>b) 
+
+if (a>c) Console.WriteLine($"max={a}");
+else 
+{
+    Console.WriteLine($"max={c}");
+}
+else
+{
+    if (b>c) Console.WriteLine($"max={b}");
+    else
+    {
+        Console.WriteLine($"max={c}");
+    }
+}
+// 3 задание
+Console.WriteLine("Input number");
+int a =Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 ==1)
+  {
+
+    Console.WriteLine($"Число {a} нечётное");
+  }  
+else
+    {
+        Console.WriteLine($"Число {a} чётное");
+    }
+// 4 задание
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int low = -N;
+
+while (low<=N)
+{
+Console.WriteLine($"{low}");
+low = low +1;
+}
